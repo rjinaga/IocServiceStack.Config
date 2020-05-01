ad86ec0df3f465f30cf1f70ee20aebebab38430d
rjinaga <rjinaga@outlook.com>
2020-05-01 13:06:48 +0530

    add few more extensions

--- IocServiceStack.Config/ConfigExtension.cs
+++ IocServiceStack.Config/ConfigExtension.cs
@@ -6,31 +6,37 @@
 
     public static class ConfigExtension
     {
-        public static void ConfigFromJsonContent(this ContainerConfig containerConfig, string appDependeciesJson)
+        public static void ConfigFromJsonContent(this ContainerOptions containerOptions, string appDependeciesJson)
         {
             var dependencies = JsonConvert.DeserializeObject<AppDependenciesObject>(appDependeciesJson);
 
+            // Add Root Assemblies
+            var modules = dependencies.AppDependencies.Modules;
+            containerOptions.Assemblies = modules == null || modules.Length == 0 ? null : modules;
+
+            if (containerOptions.Assemblies != null)
+            {
+                // Add dependecies in hirarchy
+                AddDependeciesRecursively(containerOptions, dependencies.AppDependencies.Dependencies);
+
+                // Add Shared Dependencies
+                if (dependencies.SharedDependencies != null && dependencies.SharedDependencies.Modules != null)
+                {
+                    containerOptions.AddSharedServices(shared =>
+                    {
+                        var sharedModules = dependencies.SharedDependencies.Modules;
+                        shared.Assemblies = sharedModules == null || sharedModules.Length == 0 ? null : sharedModules;
+                    });
+                }
+            }
+        }
+
+        public static void ConfigFromJsonContent(this ContainerConfig containerConfig, string appDependeciesJson)
+        {
             containerConfig.AddServices(service =>
             {
-                // Add Root Assemblies
-                var modules = dependencies.AppDependencies.Modules;
-                service.Assemblies = modules == null || modules.Length == 0 ? null : modules;
-
-                if (service.Assemblies != null)
-                {
-                    // Add dependecies in hirarchy
-                    AddDependeciesRecursively(service, dependencies.AppDependencies.Dependencies);
-
-                    // Add Shared Dependencies
-                    if (dependencies.SharedDependencies != null && dependencies.SharedDependencies.Modules != null)
-                    {
-                        service.AddSharedServices(shared =>
-                        {
-                            var sharedModules = dependencies.SharedDependencies.Modules;
-                            shared.Assemblies = sharedModules == null || sharedModules.Length == 0 ? null : sharedModules;
-                        });
-                    }
-                }
+                ConfigFromJsonContent(service, appDependeciesJson);
+                service.StrictMode = true;
             });
         }
 
@@ -38,6 +44,12 @@
         {
             var fileContent = File.ReadAllText("appdependencies.json");
             ConfigFromJsonContent(containerConfig, fileContent);
+        }
+
+        public static void ConfigFromFile(this ContainerOptions containerOptions)
+        {
+            var fileContent = File.ReadAllText("appdependencies.json");
+            ConfigFromJsonContent(containerOptions, fileContent);
         }
 
         private static void AddDependeciesRecursively(ContainerOptions containerOptions, AppDependencies dependencies)
